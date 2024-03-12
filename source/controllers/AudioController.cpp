2f54a3099efb39fa3c7ac92d3704dbb6a12fe2be
Haoxuan Zou <hz252@cornell.edu>
2024-03-12 02:47:28 -0400

    coding AudioController

--- source/controllers/AudioController.cpp
+++ source/controllers/AudioController.cpp
@@ -1,1 +1,11 @@
 #include "AudioController.h"
+
+
+bool AudioController::play(const std::string key, const std::shared_ptr<Sound> &sound, bool loop = false, float volume = 1.0f, bool force = false) {
+    if (!AudioEngine::get()->isActive(key)) {
+        AudioEngine::get() -> play(key, sound, false, sound->getVolume());
+        return true;
+    }
+    
+    return false;
+}
--- source/controllers/AudioController.h
+++ source/controllers/AudioController.h
@@ -1,7 +1,15 @@
 #ifndef __AUDIO_CONTROLLER_H__
 #define __AUDIO_CONTROLLER_H__
 
+#include <cugl/cugl.h>
+using namespace cugl;
+
 class AudioController {
     // TODO: How to do this?
+    
+    
+public:
+    bool play(const std::string key, const std::shared_ptr<Sound> &sound,
+              bool loop, float volume, bool force);
 };
 #endif
