f2d32b8e694c680ec937b45743ae76aca869fb8f
Xilai Dai <xd44@cornell.edu>
2024-03-12 02:24:08 -0400

    add character part finished

--- source/controllers/CharacterController.cpp
+++ source/controllers/CharacterController.cpp
@@ -1,1 +1,85 @@
 #include "CharacterController.h"
+
+bool CharacterController::init(const cugl::Vec2 &pos, float scale){
+    _name = "TODO_PLACEHOLDER";
+    _node = nullptr;
+    _offset = pos;
+    _drawScale = scale;
+    
+    return true;
+}
+
+void CharacterController::dispose(){
+    _node = nullptr;
+    _obstacles.clear();
+}
+
+std::shared_ptr<physics2::BoxObstacle> CharacterController::makePart(int part, int connect, const Vec2& pos) {
+    std::shared_ptr<Texture> image = _textures[part];
+    Size size = image->getSize();
+    size.width /= _drawScale;
+    size.height /= _drawScale;
+
+    Vec2 pos2 = pos;
+    if (connect != PART_NONE) {
+        pos2 += _obstacles[connect]->getPosition();
+    }
+
+    std::shared_ptr<physics2::BoxObstacle> body = physics2::BoxObstacle::alloc(pos2, size);
+    // log all information
+    CULog("part: %d", part);
+    CULog("connect: %d", connect);
+    CULog("pos: %f, %f", pos.x, pos.y);
+    CULog("pos2: %f, %f", pos2.x, pos2.y);
+    CULog("size: %f, %f", size.width, size.height);
+    body->setName(getPartName(part));
+    body->setDensity(DEFAULT_DENSITY);
+    
+    _obstacles.push_back(body);
+    return body;
+}
+
+bool CharacterController::buildParts(const std::shared_ptr<AssetManager>& assets){
+    CUAssertLog(_obstacles.empty(), "Bodies are already initialized");
+    
+    // Get the images from the asset manager
+        bool success = true;
+        for(int ii = 0; ii <= PART_LH; ii++) {
+            std::string name = getPartName(ii);
+            std::shared_ptr<Texture> image = assets->get<Texture>(name);
+            if (image == nullptr) {
+                success = false;
+            } else {
+                _textures.push_back(image);
+            }
+        }
+        if (!success) {
+            return false;
+        }
+    
+    return true;
+}
+
+
+std::string CharacterController::getPartName(int part){
+    switch (part) {
+        case PART_BODY:
+            return BODY_TEXTURE;
+        case PART_RH:
+        case PART_LH:
+            return HAND_TEXTURE;
+        case PART_JR1:
+        case PART_JR2:
+        case PART_JR3:
+        case PART_JR4:
+        case PART_JR5:
+        case PART_LR1:
+        case PART_LR2:
+        case PART_LR3:
+        case PART_LR4:
+        case PART_LR5:
+            return CJOINT_TEXTURE;
+        default:
+            return "UNKNOWN";
+    }
+}
--- source/controllers/CharacterController.h
+++ source/controllers/CharacterController.h
@@ -5,7 +5,108 @@
 #include <vector>
 #include "../models/CharacterModel.h"
 
+
+
+
+/*
+ 
+ O--o--oo--o--o()o--o--oo--o--O
+ 
+ 
+ 
+ */
+
+#define PART_NONE   -1
+#define PART_BODY   0
+#define PART_JR1    1
+#define PART_JR2    2
+#define PART_JR3    3
+#define PART_JR4    4
+#define PART_JR5    5
+#define PART_RH     6
+#define PART_LR1    7
+#define PART_LR2    8
+#define PART_LR3    9
+#define PART_LR4    10
+#define PART_LR5    11
+#define PART_LH     12
+
+#define BODY_TEXTURE "body1"
+#define CJOINT_TEXTURE "j1"
+#define HAND_TEXTURE "hand1"
+
+#define CJOINT_OFFSET 1.0f
+
+
+#pragma mark -
+#pragma mark Physics Constants
+/** The density for each body part */
+#define DEFAULT_DENSITY  1.0f
+/** The density for the center of mass */
+#define CENTROID_DENSITY 0.1f
+/** The radius for the center of mass */
+#define CENTROID_RADIUS  0.1f
+
+
+using namespace cugl;
+
 class CharacterController {
+protected:
+    std::string _name;
+    std::vector<std::shared_ptr<cugl::Texture>> _textures;
+    std::vector<std::shared_ptr<cugl::physics2::Obstacle>> _obstacles;
+    std::vector<std::shared_ptr<cugl::physics2::Joint>> _joints;
     
+    cugl::Vec2 _offset;
+    std::shared_ptr<cugl::scene2::SceneNode> _node;
+    cugl::Mat4 _affine;
+    float _drawScale;
+    
+    CharacterController(void) : _drawScale(0) { }
+    virtual ~CharacterController(void) { dispose(); }
+    void dispose();
+    
+    std::string getPartName(int part);
+    
+    std::shared_ptr<cugl::physics2::BoxObstacle> makePart(int part, int connect, const cugl::Vec2& pos);
+    
+    bool init() {
+        return init(cugl::Vec2::ZERO, 1.0f);
+    }
+    
+    bool init(const cugl::Vec2& pos) {
+        return init(pos,1.0f);
+    }
+    
+    bool init(const cugl::Vec2& pos, float scale);
+    
+#pragma mark -
+#pragma mark Static Constructors
+    
+    static std::shared_ptr<CharacterController> alloc() {
+        std::shared_ptr<CharacterController> result = std::make_shared<CharacterController>();
+        return (result->init() ? result : nullptr);
+    }
+    static std::shared_ptr<CharacterController> alloc(const cugl::Vec2& pos) {
+        std::shared_ptr<CharacterController> result = std::make_shared<CharacterController>();
+        return (result->init(pos, 1.0f) ? result : nullptr);
+    }
+    static std::shared_ptr<CharacterController> alloc(const cugl::Vec2& pos, float scale) {
+        std::shared_ptr<CharacterController> result = std::make_shared<CharacterController>();
+        return (result->init(pos, scale) ? result : nullptr);
+    }
+    
+    void activate(const std::shared_ptr<cugl::physics2::net::NetWorld>& world);
+    void deactivate(const std::shared_ptr<cugl::physics2::net::NetWorld>& world);
+    
+    bool createJoints();
+    bool buildParts(const std::shared_ptr<cugl::AssetManager>& assets);
+    
+    void setSceneNode(const std::shared_ptr<cugl::scene2::SceneNode>& node);
+    void setDrawScale(float scale);
+    
+    bool moveRightHand(cugl::Vec2 offset);
+    bool moveLeftHand(cugl::Vec2 offset);
 };
+
 #endif /* CharacterController_h */
