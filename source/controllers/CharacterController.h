b26f0354f7a8209a5c299295c3f27d1e72aedd16
PomeloBranco <126112573+PomeloBranco@users.noreply.github.com>
2024-03-25 22:10:22 -0400

    Complete Body Part's Animation

--- source/controllers/CharacterController.h
+++ source/controllers/CharacterController.h
@@ -29,11 +29,10 @@
 // #define PART_LR6    13
 // #define PART_LH     14
 
-#define PART_NONE   -1
-#define PART_BODY   0
-#define PART_RH     1
-#define PART_LH     2
-
+#define PART_NONE -1
+#define PART_BODY 0
+#define PART_RH 1
+#define PART_LH 2
 
 #define BODY_TEXTURE "body192"
 #define CJOINT_TEXTURE "joint32"
@@ -41,29 +40,33 @@
 #define L_HAND_TEXTURE "lhand64"
 #define R_HAND_TEXTURE "rhand64"
 
-
 using namespace cugl;
 
-class CharacterController {
+class CharacterController
+{
 protected:
 	std::string _name;
 	std::vector<std::shared_ptr<cugl::Texture>> _textures;
-    std::shared_ptr<cugl::Texture> _blacktextures;
-    std::shared_ptr<cugl::Texture> _pinktextures;
-    std::shared_ptr<cugl::Texture> _bluetextures;
-    std::shared_ptr<cugl::Texture> _greentextures;
-    std::shared_ptr<cugl::Texture> _orangetextures;
-    std::shared_ptr<cugl::Texture> _purpletextures;
-    std::shared_ptr<cugl::Texture> _orangedecoration;
+	std::shared_ptr<cugl::Texture> _blacktextures;
+	std::shared_ptr<cugl::Texture> _pinktextures;
+	std::shared_ptr<cugl::Texture> _bluetextures;
+	std::shared_ptr<cugl::Texture> _greentextures;
+	std::shared_ptr<cugl::Texture> _orangetextures;
+	std::shared_ptr<cugl::Texture> _purpletextures;
+	std::shared_ptr<cugl::Texture> _orangedecoration;
+	std::shared_ptr<cugl::Texture> _bluedecoration;
+	std::shared_ptr<cugl::Texture> _greendecoration;
+	std::shared_ptr<cugl::Texture> _pinkdecoration;
+	std::shared_ptr<cugl::Texture> _purpledecoration;
 	std::vector<std::shared_ptr<cugl::physics2::Obstacle>> _obstacles;
 	std::vector<std::shared_ptr<cugl::physics2::Joint>> _joints;
-    /** The physics world; part of the model (though listeners elsewhere) */
-    std::shared_ptr<cugl::physics2::net::NetWorld> _world;
-    
-    std::shared_ptr<cugl::scene2::ActionManager> _actions;
-    std::shared_ptr<cugl::scene2::Animate> _animate;
-    std::shared_ptr<cugl::scene2::Animate> _blackanimate;
-    std::string _color;
+	/** The physics world; part of the model (though listeners elsewhere) */
+	std::shared_ptr<cugl::physics2::net::NetWorld> _world;
+
+	std::shared_ptr<cugl::scene2::ActionManager> _actions;
+	std::shared_ptr<cugl::scene2::Animate> _animate;
+	std::shared_ptr<cugl::scene2::Animate> _blackanimate;
+	std::string _color;
 
 	cugl::Vec2 _offset;
 	std::shared_ptr<cugl::scene2::SceneNode> _node;
@@ -71,7 +74,7 @@ protected:
 	float _drawScale;
 	std::string getPartName(int part);
 	bool _motorEnabled = true;
-	std::shared_ptr<cugl::physics2::Obstacle> makePart(int part, int connect, const cugl::Vec2& pos);
+	std::shared_ptr<cugl::physics2::Obstacle> makePart(int part, int connect, const cugl::Vec2 &pos);
 	Vec2 SolveAngleMiddleBisectorLine(Vec2 p);
 	Vec2 armMiddleExp_R(Vec2 end);
 	std::vector<std::shared_ptr<cugl::physics2::Joint>> dummy_jointsmaker(std::vector<int> part_indices);
@@ -92,52 +95,57 @@ protected:
 	std::shared_ptr<physics2::MotorJoint> rightArmJoint;
 	std::shared_ptr<physics2::MotorJoint> leftForearmJoint;
 	std::shared_ptr<physics2::MotorJoint> rightForearmJoint;
-    
-    
-    std::vector<std::shared_ptr<scene2::PolygonNode>> _lineNodes;
-    std::shared_ptr<scene2::SpriteNode> _bodyNode;
-    std::shared_ptr<scene2::SpriteNode> _decorationNode;
+
+	std::vector<std::shared_ptr<scene2::PolygonNode>> _lineNodes;
+	std::shared_ptr<scene2::SpriteNode> _bodyNode;
+	std::shared_ptr<scene2::SpriteNode> _decorationNode;
 	std::shared_ptr<scene2::PolygonNode> _LHNode;
 	std::shared_ptr<scene2::PolygonNode> _RHNode;
+
 public:
-    CharacterController(void);
-    virtual ~CharacterController(void);
+	CharacterController(void);
+	virtual ~CharacterController(void);
 	void dispose();
 
-	bool init() {
+	bool init()
+	{
 		return init(cugl::Vec2::ZERO, 1.0f);
 	}
 
-	bool init(const cugl::Vec2& pos) {
+	bool init(const cugl::Vec2 &pos)
+	{
 		return init(pos, 1.0f);
 	}
 
-	bool init(const cugl::Vec2& pos, float scale);
-    void update(float timestep);
-    void setColor(std::string color);
+	bool init(const cugl::Vec2 &pos, float scale);
+	void update(float timestep);
+	void setColor(std::string color);
 
 #pragma mark Static Constructors
 
-	static std::shared_ptr<CharacterController> alloc() {
+	static std::shared_ptr<CharacterController> alloc()
+	{
 		std::shared_ptr<CharacterController> result = std::make_shared<CharacterController>();
 		return (result->init() ? result : nullptr);
 	}
-	static std::shared_ptr<CharacterController> alloc(const cugl::Vec2& pos) {
+	static std::shared_ptr<CharacterController> alloc(const cugl::Vec2 &pos)
+	{
 		std::shared_ptr<CharacterController> result = std::make_shared<CharacterController>();
 		return (result->init(pos, 1.0f) ? result : nullptr);
 	}
-	static std::shared_ptr<CharacterController> alloc(const cugl::Vec2& pos, float scale) {
+	static std::shared_ptr<CharacterController> alloc(const cugl::Vec2 &pos, float scale)
+	{
 		std::shared_ptr<CharacterController> result = std::make_shared<CharacterController>();
 		return (result->init(pos, scale) ? result : nullptr);
 	}
 
-	void activate(const std::shared_ptr<cugl::physics2::net::NetWorld>& world);
-	void deactivate(const std::shared_ptr<cugl::physics2::net::NetWorld>& world);
+	void activate(const std::shared_ptr<cugl::physics2::net::NetWorld> &world);
+	void deactivate(const std::shared_ptr<cugl::physics2::net::NetWorld> &world);
 
 	bool createJoints();
-	bool buildParts(const std::shared_ptr<cugl::AssetManager>& assets);
+	bool buildParts(const std::shared_ptr<cugl::AssetManager> &assets);
 
-	void setSceneNode(const std::shared_ptr<cugl::scene2::SceneNode>& node);
+	void setSceneNode(const std::shared_ptr<cugl::scene2::SceneNode> &node);
 	void setDrawScale(float scale);
 
 	void enableMotor();
@@ -152,14 +160,16 @@ public:
 	bool moveRightHand(cugl::Vec2 offset);
 	bool moveLeftHand(cugl::Vec2 offset);
 
-	Vec2 getLeftHandPosition() {
+	Vec2 getLeftHandPosition()
+	{
 		auto p = _obstacles[PART_LH]->getPosition();
-		//CULog("Left Hand Position: %f, %f", p.x, p.y);
+		// CULog("Left Hand Position: %f, %f", p.x, p.y);
 		return p;
 	}
-	Vec2 getRightHandPosition() {
+	Vec2 getRightHandPosition()
+	{
 		auto p = _obstacles[PART_RH]->getPosition();
-		//CULog("Right Hand Position: %f, %f", p.x, p.y);
+		// CULog("Right Hand Position: %f, %f", p.x, p.y);
 		return p;
 	}
 	const std::vector<std::shared_ptr<cugl::physics2::Obstacle>> getObstacles() const { return _obstacles; }
@@ -183,11 +193,11 @@ public:
 	 *
 	 * @param node  The scene graph node representing this Ragdoll, which has been added to the world node already.
 	 */
-	void linkPartsToWorld(const std::shared_ptr<cugl::physics2::net::NetWorld>& _world, const std::shared_ptr<cugl::scene2::SceneNode>& _scenenode, float _scale);
-    
-    void drawCharacterLines(float scale);
-    
-    void drawDecoration(float scale);
+	void linkPartsToWorld(const std::shared_ptr<cugl::physics2::net::NetWorld> &_world, const std::shared_ptr<cugl::scene2::SceneNode> &_scenenode, float _scale);
+
+	void drawCharacterLines(float scale);
+
+	void drawDecoration(float scale);
 };
 
 #endif /* CharacterController_h */
