72f23756be6f9ba7ba1b051787a84f0b9a2fd7a0
Haoxuan Zou <hz252@cornell.edu>
2024-04-19 11:12:11 -0400

    fixing map resize issue

--- assets/tileset/demo_elements.tsx
+++ assets/tileset/demo_elements.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="ragdoll_elements" tilewidth="3718" tileheight="4483" tilecount="29" columns="0" objectalignment="bottomleft">
+<tileset version="1.10" tiledversion="1.10.2" name="ragdoll_elements" tilewidth="3718" tileheight="5000" tilecount="30" columns="0" objectalignment="bottomleft">
  <editorsettings>
   <export target="ragdoll_elements..tsx" format="tsx"/>
  </editorsettings>
@@ -92,12 +92,15 @@
   <image width="1280" height="720" source="../textures/titlebackground.png"/>
  </tile>
  <tile id="40">
-  <image width="1278" height="4483" source="../textures/lv3_background_1.png"/>
+  <image width="1600" height="4480" source="../textures/lv3_background_1.png"/>
  </tile>
  <tile id="41">
-  <image width="1600" height="3840" source="../textures/lv2_background_1.png"/>
+  <image width="1560" height="5000" source="../textures/lv2_background_1.png"/>
  </tile>
  <tile id="42">
   <image width="1600" height="3840" source="../textures/level4/lv4_background_1.png"/>
+ </tile>
+ <tile id="43">
+  <image width="3718" height="580" source="../textures/lv1_background_1.png"/>
  </tile>
 </tileset>
