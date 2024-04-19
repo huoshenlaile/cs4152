d9c7641a47602aa01bff6f49ab458eeeb5d9d1f2
Xilai Dai <xd44@cornell.edu>
2024-04-19 09:15:19 -0400

    change sensor trigger

--- source/objects/Sensor.cpp
+++ source/objects/Sensor.cpp
@@ -1,4 +1,9 @@
 #include "Sensor.h"
+/*
+
+    SENSOR CLASS IS UPDATED on Monday afternoon, it will break some behavior. please check the google doc for usage. 
+
+*/
 bool Sensor::init(const std::shared_ptr<cugl::JsonValue>& json, Vec2 scale, Rect bounds){
     // call super.init
     Interactable::init(json, scale, bounds);
@@ -8,9 +13,11 @@ bool Sensor::init(const std::shared_ptr<cugl::JsonValue>& json, Vec2 scale, Rect
     for (int i = 0; i < properties->size(); i++){
         if (properties->get(i)->getString("propertytype") == "PubMessage"){
             auto pub = properties->get(i)->get("value");
-            message_head = pub->getString("Head");
-            pub_message_body = pub->getString("Body");
-            break;
+            if (pub->getBool("Enabled", true) && (pub->getString("Trigger") != "" )){
+                triggers.push_back(pub->getString("Trigger"));
+                message_heads.push_back(pub->getString("Head"));
+                message_bodies.push_back(pub->getString("Body"));
+            }
         }
     }
     activated = true;
@@ -20,15 +27,14 @@ bool Sensor::init(const std::shared_ptr<cugl::JsonValue>& json, Vec2 scale, Rect
 }
 
 PublishedMessage Sensor::onBeginContact(std::shared_ptr<cugl::physics2::Obstacle> other, b2Contact* contact, std::shared_ptr<Interactable> otherInteractable, bool isCharacter, std::shared_ptr<CharacterController> character){
-    CULog("Sensor of [%s] tested", message_head.c_str());
-    if (isCharacter){
-        // if the sensor is activated
-        if (activated){
-            if (other->getName() == "body192"){
-                CULog("Sensor of [%s] activated", message_head.c_str());
+    if (activated){
+        auto other_name = other->getName();
+        for (int i = 0; i < triggers.size(); i++){
+            if (other_name == triggers[i]){
                 auto a = PublishedMessage();
-                a.Head = message_head;
-                a.Body = pub_message_body;
+                a.Head = message_heads[i];
+                CULog("Sensor %s triggered", a.Head.c_str());
+                a.Body = message_bodies[i];
                 return a;
             }
         }
