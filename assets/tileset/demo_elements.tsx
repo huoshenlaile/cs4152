34a0b0194e18de9558a246f5f77ab9dc63a42de9
PomeloBranco <126112573+PomeloBranco@users.noreply.github.com>
2024-05-15 15:49:55 -0400

    Fix Lv9 Scale

--- assets/tileset/demo_elements.tsx
+++ assets/tileset/demo_elements.tsx
@@ -8,37 +8,37 @@
   <properties>
    <property name="properties" value=""/>
   </properties>
-  <image width="447" height="138" source="lv1_palette.png"/>
+  <image source="lv1_palette.png"/>
  </tile>
  <tile id="9" type="wall">
   <properties>
    <property name="properties" value=""/>
   </properties>
-  <image width="214" height="100" source="../textures/popleft.png"/>
+  <image source="../textures/popleft.png"/>
   <objectgroup draworder="index" id="2">
    <object id="1" x="9.46814" y="10.0604" width="172.615" height="84.5282"/>
   </objectgroup>
  </tile>
  <tile id="10">
-  <image width="100" height="100" source="../textures/goalpop.png"/>
+  <image source="../textures/goalpop.png"/>
  </tile>
  <tile id="11">
-  <image width="214" height="100" source="../textures/popright.png"/>
+  <image source="../textures/popright.png"/>
  </tile>
  <tile id="12">
-  <image width="1500" height="700" source="../textures/popart_platformbubble_left.png"/>
+  <image source="../textures/popart_platformbubble_left.png"/>
  </tile>
  <tile id="13">
-  <image width="1500" height="700" source="../textures/popart_platformbubble_right.png"/>
+  <image source="../textures/popart_platformbubble_right.png"/>
  </tile>
  <tile id="14">
-  <image width="2501" height="301" source="../textures/popart_platformLine.png"/>
+  <image source="../textures/popart_platformLine.png"/>
  </tile>
  <tile id="15">
-  <image width="700" height="700" source="../textures/popart_goal.png"/>
+  <image source="../textures/popart_goal.png"/>
  </tile>
  <tile id="16">
-  <image width="2340" height="1080" source="../textures/popart_background.png"/>
+  <image source="../textures/popart_background.png"/>
  </tile>
  <tile id="24">
   <image width="232" height="476" source="../textures/alpharelease_soy_capon.png"/>
@@ -77,7 +77,7 @@
   <image width="337" height="1724" source="../textures/tube_curved_blue.png"/>
  </tile>
  <tile id="36">
-  <image width="103" height="50" source="../textures/spikes.png"/>
+  <image source="../textures/spikes.png"/>
  </tile>
  <tile id="37">
   <image width="180" height="99" source="../textures/nogravityplatform_blackhole.png"/>
@@ -165,5 +165,8 @@
  </tile>
  <tile id="79">
   <image width="232" height="476" source="../textures/paintingbottle/soy_black.png"/>
+ </tile>
+ <tile id="80">
+  <image width="175" height="52" source="../textures/movingplatform_comet.png"/>
  </tile>
 </tileset>
